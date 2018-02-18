82f7d1d624506c033e0f673d063bcb352a07fc96
TheFloGroup <34571095+TheFloGroup@users.noreply.github.com>
2018-02-18 00:13:49 +0000

    WEBSITE DESIGNED BY HARRY

---
+++ index.php
@@ -0,0 +1,69 @@
+<html class="js"><head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>The Flo Group</title>
+	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="assets/css/venobox.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W4DFG7V');</script>
+<!-- End Google Tag Manager -->
+</head>
+
+
+<body>
+<br><br><br><br><br>
+	<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W4DFG7V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+	<div class="container">
+		<img src="assets/img/logo.png" class="animated infinite swing" alt="The Flo Group" id="logo">
+<br><br>
+		<div id="buttons">
+			<a class="home"href="https://www.theflo.group" title=""></a>
+			<a class="buy" href="https://www.ye-marketplace.org.uk/Products" title=""></a>
+			<a class="About" data-title="Our Company" data-gall="About Our Company" data-type="iframe" href="#"></a>
+			<div style="display:none;">
+				<a class="About" data-title="Our Company" data-gall="About Our Company" data-type="iframe" href="#"></a>
+				<a class="About" data-title="Our Company" data-gall="About Our Company" data-type="iframe" href="#"></a>
+				<a class="About" data-title="Our Company" data-gall="About Our Company" data-type="iframe" href="#"></a>
+			</div>
+
+		</div>                       
+
+		</div>
+	</div>
+	
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> <center>
+
+<!-- Footer -->
+<!--
+<footer id="footer">
+<ul class="icons">
+	<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+	<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+	<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+	<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+	<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+	</ul>
+	<ul class="copyright">
+	<h4>© TheFloGroup</h4>
+	<p>Our Company is based at Colchester Royal Grammar School</p>
+	</ul>
+	</footer>
+-->
+</center>
+	
+<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="assets/js/clipboard.min.js"></script>
+<script src="assets/js/venobox.min.js"></script>
+
+</body>
+</html>
